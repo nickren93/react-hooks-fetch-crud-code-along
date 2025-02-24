4a4086e65ef4d3ab0c9635acb39b4761594e5bf8
Sitong <nickren93@gmail.com>
2025-02-23 21:52:54 -0500

    complete lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -7,8 +7,38 @@ function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
+  // Add useEffect hook
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
+
+   // add this function!
+   function handleAddItem(newItem) {
+    setItems([...items, newItem]);
+  }
+
   function handleCategoryChange(category) {
     setSelectedCategory(category);
+  }
+
+  // add this callback function
+  function handleUpdateItem(newItem) {
+    //console.log("In ShoppingCart:", updatedItem);
+    const newItems = items.map(item =>{
+      if(item.id == newItem.id){
+        return newItem
+      }else{
+        return item
+      }
+    })
+    setItems(newItems)
+  }
+
+  function handleDelete(itemToDelete){
+    const updatedItems = items.filter(item => item.id != itemToDelete.id)
+    setItems(updatedItems)
   }
 
   const itemsToDisplay = items.filter((item) => {
@@ -19,14 +49,15 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateItem} 
+          onHandleDelete={handleDelete}/>
         ))}
       </ul>
     </div>
